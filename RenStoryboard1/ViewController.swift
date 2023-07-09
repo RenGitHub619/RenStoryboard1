4ff11d34518a6216624496e1cf85267e102d56af
ren <a12784002@gmail.com>
2023-07-09 10:32:26 +0800

    Initial Commit

---
+++ RenStoryboard1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RenStoryboard1
+//
+//  Created by 盧宥任 on 2023/7/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
